620ee543844322ecaa41f61c20545f2ae6be6d25
Ryan Halliwell <git@rayliwell.com>
2024-09-08 13:16:24 +0100

    chore: add nextjs

---
+++ prettier.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('prettier').Config} */
+export default {
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'strict',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  insertPragma: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
